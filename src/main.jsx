a28c3847f42e7d8a167df117b7ab1688770e558e
Michael Hughes <microneesia79@gmail.com>
2026-06-26 22:03:12 -0700

    Render share workspace panel

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,20 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import PwaStatus from './PwaStatus.jsx';
 import KeyboardHelp from './KeyboardHelp.jsx';
+import SharePanel from './SharePanel.jsx';
 import { registerServiceWorker } from './registerServiceWorker.js';
 import './styles.css';
 import './graph.css';
 import './unit.css';
 import './pwa.css';
+import './share.css';
 import './accessibility.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <PwaStatus />
+    <SharePanel />
     <KeyboardHelp />
   </React.StrictMode>,
 );

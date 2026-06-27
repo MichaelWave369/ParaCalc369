0491ebe8c05dc3a996c8324e84ad2384019241c2
Michael Hughes <microneesia79@gmail.com>
2026-06-26 21:57:29 -0700

    Render keyboard help overlay

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import PwaStatus from './PwaStatus.jsx';
+import KeyboardHelp from './KeyboardHelp.jsx';
 import { registerServiceWorker } from './registerServiceWorker.js';
 import './styles.css';
 import './graph.css';
 import './unit.css';
 import './pwa.css';
+import './accessibility.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <PwaStatus />
+    <KeyboardHelp />
   </React.StrictMode>,
 );
 

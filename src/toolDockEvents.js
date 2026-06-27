3eaa92248cbca052775befbe39f917892c1a609d
Michael Hughes <microneesia79@gmail.com>
2026-06-26 23:32:23 -0700

    Add expression insert event

--- src/toolDockEvents.js
+++ src/toolDockEvents.js
@@ -2,9 +2,14 @@ export const TOOL_EVENTS = {
   formulas: 'paracalc:open-formulas',
   history: 'paracalc:open-history-vault',
   help: 'paracalc:open-keyboard-help',
+  expression: 'paracalc:set-expression',
 };
 
 export function openTool(tool) {
   const eventName = TOOL_EVENTS[tool];
   if (eventName) window.dispatchEvent(new CustomEvent(eventName));
+}
+
+export function insertExpression(expression) {
+  window.dispatchEvent(new CustomEvent(TOOL_EVENTS.expression, { detail: { expression } }));
 }

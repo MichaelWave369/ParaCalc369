94da7b78ac3bc90a48afbe1d7f9d0d36213fef16
Michael Hughes <microneesia79@gmail.com>
2026-06-26 22:11:51 -0700

    Render Formula Library overlay

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import App from './App.jsx';
 import PwaStatus from './PwaStatus.jsx';
 import KeyboardHelp from './KeyboardHelp.jsx';
 import SharePanel from './SharePanel.jsx';
+import FormulaLibrary from './FormulaLibrary.jsx';
 import { registerServiceWorker } from './registerServiceWorker.js';
 import './styles.css';
 import './graph.css';
 import './unit.css';
 import './pwa.css';
 import './share.css';
+import './formula.css';
 import './accessibility.css';
 
 createRoot(document.getElementById('root')).render(
@@ -17,6 +19,7 @@ createRoot(document.getElementById('root')).render(
     <App />
     <PwaStatus />
     <SharePanel />
+    <FormulaLibrary />
     <KeyboardHelp />
   </React.StrictMode>,
 );

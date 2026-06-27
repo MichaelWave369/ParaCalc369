790bc3b81a99df1f02e4d31ca60665441db945f9
Michael Hughes <microneesia79@gmail.com>
2026-06-26 21:36:03 -0700

    Render PWA status component

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import PwaStatus from './PwaStatus.jsx';
 import { registerServiceWorker } from './registerServiceWorker.js';
 import './styles.css';
 import './graph.css';
@@ -10,6 +11,7 @@ import './pwa.css';
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <PwaStatus />
   </React.StrictMode>,
 );
 
